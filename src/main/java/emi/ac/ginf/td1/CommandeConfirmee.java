a5d100f22603c0d653471ad6b700e5ff3d31c91c
OthamaneELBOUR <elbour.othmane@gmail.com>
2024-04-29 17:07:33 +0100

    Updating class CommandeConfirmee

--- src/main/java/emi/ac/ginf/td1/CommandeConfirmee.java
+++ src/main/java/emi/ac/ginf/td1/CommandeConfirmee.java
@@ -6,8 +6,12 @@ public class CommandeConfirmee implements CommandeState{
     public void traiterCommande(Commande c) {
         System.out.println("Commandes pretes à être livrée");
         c.factureCommande();
+        System.out.println("Commande Enregistrée");
+    }
+
+    public void livrerCommande(Commande c) {
         c.setDateLivraison(LocalDate.now());
-        System.out.println("Commande Enregistrée et bien livrée");
+        System.out.println("Commande Livrée");
     }
     
 }
